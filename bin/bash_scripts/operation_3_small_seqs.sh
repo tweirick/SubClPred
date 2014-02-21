4b5903b2b5d7c8b6b7128f6fcc2c110f003e00e8
tweirick <tyler.weirick@gmail.com>
2014-02-21 11:17:36 -0600

    Update method script 3

--- bin/bash_scripts/operation_3_small_seqs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This script 
-
-python bin/python_scripts/remove_fastas_less_than_x_chars.py --file_set data/2013-12-10.1_laccase_faa/lac_pt.faa --min_chars 200 --out_dir data/2013-12-10.3_cleaned_laccases_faa/
-
-
